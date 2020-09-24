df92e96c2e9265ffb26c3003cd743be5c92133d5
Michael Lustig <michael.lustig@walmart.com>
2020-09-24 12:07:56 -0400

    Create types

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+export type MenuItem = {
+  name: string;
+  seconds: number;
+};
+
+export type Menu = {
+  items: Array<MenuItem>;
+};
+
+export type BaristaStatus = "idle" | "working";
+
+export type TicketId = string;
+
+export type Queue = {
+  drinks: Array<MenuItem>;
+};
+
+export type Barista = {
+  status: BaristaStatus;
+  current: TicketId;
+};
+
+export type Ticket = {
+  id: TicketId;
+  item: MenuItem;
+};
+
+export type Counter = {
+  drinks: Array<MenuItem>;
+};
